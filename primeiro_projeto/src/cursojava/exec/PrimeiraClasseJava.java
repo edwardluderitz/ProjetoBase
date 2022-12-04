f5d5c98d24b430e506ed594d7c9e1261ad549b3b
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-12-04 18:12:52 -0300

    Teste

--- primeiro_projeto/src/cursojava/exec/PrimeiraClasseJava.java
+++ primeiro_projeto/src/cursojava/exec/PrimeiraClasseJava.java
@@ -5,7 +5,5 @@ public class PrimeiraClasseJava {
 	/*Main é um método auto executavel em Java*/
 	public static void main(String[] args) {
 		System.out.println("meu primeiro programa em java");
-		System.out.println();
 	}
-
 }
